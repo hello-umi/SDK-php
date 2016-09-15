3e721c6b965f7eca3df8d5781a629b45b8be36b4
Antoni Espinosa Polo <antoniespinosa@me.com>
2016-09-15 16:33:16 +0200

    Changes

--- index.php
+++ index.php
@@ -2,8 +2,8 @@ Prueba test
 <br>
 <?php
     include_once "helloumi/helloumi.php";
-    $helloumi = new Helloumi("9b9c7c261c71cb65165789bcee2e759d941dd8c2");
-    $customer = $helloumi->getCustomer("34606665706");
+    $helloumi = new Helloumi("3c46513a81802fdea0574a9e7db03d9780a69363");
+    $customer = $helloumi->getCustomerFromPhone("34606665706");
     echo $customer->getName();
     $helloumi->sendPhone("34606665706", "Mensaje de pruebas");
     $customer->send("Mensaje de pruebas desde customer");
